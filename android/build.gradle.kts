b6836f310a51c39f869920658df230628eeddfa9
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-17 00:58:34 +0100

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -12,11 +5,22 @@ allprojects {
     }
 }
 
-// 🔥 FORCE STABLE KOTLIN VERSION
+// 🔥 إجبار كل dependencies تستخدم Kotlin 2.0.0 فقط
 configurations.all {
     resolutionStrategy {
-        force("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0")
+        force("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.0.0")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0")
+
+        eachDependency {
+            if (requested.group == "org.jetbrains.kotlin") {
+                useVersion("2.0.0")
+            }
+        }
     }
+}
+
+// build clean task
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,8 +20,8 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 
-    // 🔥 FIX KOTLIN (STOP 2.2.0 PROBLEMS)
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // 🔥 FIX: تثبيت Kotlin على 2.0.0 (لا 2.2.0)
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 
     id("com.android.application") version "8.7.3" apply false
 }
