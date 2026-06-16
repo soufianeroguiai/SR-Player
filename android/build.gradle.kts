eefaaf6228e8aa21f78115158f29c9a70bbbdece
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-16 23:14:17 +0100

    lov

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,2 +1,6 @@
-// هذا الملف يُترك فارغاً عن قصد.
-// كل تعريفات الإضافات (AGP, Kotlin, Flutter) موجودة في settings.gradle.kts.
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
