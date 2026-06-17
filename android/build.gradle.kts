87bc86f54346b302efa1c82304a351945ca0c98d
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-17 04:30:15 +0100

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")   // 👈 تم التحديث إلى 2.2.0
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
 }
 
@@ -15,8 +15,6 @@ allprojects {
         mavenCentral()
     }
 }
-
-// ❌ تم حذف قسم resolutionStrategy بالكامل - لم يعد ضرورياً ويسبب التعارض
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
