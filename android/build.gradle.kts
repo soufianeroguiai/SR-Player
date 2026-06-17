b761586f236d23c18cd34478be627550aed82c44
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-17 02:04:29 +0100

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,5 @@
-// هذا ملف root build.gradle.kts (مهم لتثبيت Kotlin بشكل صحيح)
-
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
