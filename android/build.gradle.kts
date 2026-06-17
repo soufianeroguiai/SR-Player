8e727d09c9d58607671bed30031c4983b9d5fcb0
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-17 02:24:05 +0100

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,16 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
